516ece8999b8b829f4ab51f8936668b7ee3c2238
Roberto <105317145+ItzRoBeerT@users.noreply.github.com>
2023-01-17 21:48:10 +0100

    Added prop to the task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Expenses from './components/Expenses';
 
 function App() {
+  const expenses = [
+    {id:'e1', title: "Toilet Paper", amount: 94.5, date: new Date(2021, 4, 21) },
+    {id:'e2', title: "New TV", amount: 799.49, date: new Date(2021, 3, 14) },
+    {id:'e3', title: "Car Insurance", amount: 273.5, date: new Date(2021, 8, 4) },
+    {id:'e4', title: "New Desk (Wooden)", amount: 100.50, date: new Date(2021, 1, 6) },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenses/>
+      <Expenses items={expenses}/>
     </div>
   );
 }
